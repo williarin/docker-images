77c01ec0d80bd4c3f251f63fa686503263b09fde
William Arin <williamarin.dev@gmail.com>
2021-06-27 23:06:21 +0800

    fix: don't try to add www-data in Alpine 3.14 as it already exists

--- php/7.4/docker/scripts/ensure-www-data.sh
+++ php/7.4/docker/scripts/ensure-www-data.sh
@@ -2,7 +2,11 @@
 set -ex
 
 # ensure www-data user exists
-addgroup -g 82 -S www-data
+# note: Alpine 3.14 base image already includes www-data group
+if [[ "$ALPINE_VERSION" = "3.13" ]] ; then 
+    addgroup -g 82 -S www-data
+fi
+
 adduser -u 82 -D -S -G www-data www-data
 # 82 is the standard uid/gid for "www-data" in Alpine
 # http://git.alpinelinux.org/cgit/aports/tree/main/apache2/apache2.pre-install?h=v3.3.2
